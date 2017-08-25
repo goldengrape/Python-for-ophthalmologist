9ef73a6877df13e79fde0a28b3716a99083aa210
GoldenGrape <goldengrape@goldengrape.org>
2017-08-25 12:39:47 +0800

    开始写此教程

---
+++ lesson_0_of_python_tutorial_for_ophthalmologist.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# 面向眼科医生的Python教程(0)
+
+# # 为什么要学编程
+# 
+# 点击下面这个视频, 先看一段非眼科"成功人士"对为什么要学编程的理解: 
+# [Why Programming Is Important?](https://www.youtube.com/watch?v=Dv7gLpW91DM)
+# 
+
+# In[2]:
+
+from IPython.display import HTML
+
+# Youtube
+HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/Dv7gLpW91DM" frameborder="0" allowfullscreen></iframe>')
+
+
+
+# ## 为什么眼科医生要学习编程
+# 
+# ### 人工智能时代与眼科
+
+# ## 女医生也可以学编程么? 
+
+# ## 我应该如何学习编程
+
+# # 为什么要学Python
+
+# # 如何开始
+
+# # 参考资料
+
+# In[ ]:
+
+
+
